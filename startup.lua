c63606444299cc1b0cce44f2eeb30f6c637585a3
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-09 14:46:05 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,1 +1,10 @@
+path_cmd = "/"
+running = true
 
+while running do
+    -- Main 
+    local cmd = read(path_cmd .. "&: ")
+    if cmd == "close" or cmd == "exit" then
+        running = false
+    end
+end
