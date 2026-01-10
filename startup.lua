955d81c90ad8e5d49c02193dd9808ebd24a0e1ec
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-10 17:56:19 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -5,3 +5,25 @@ gpu.fill(0x0000FF)
 gpu.filledRectangle(2, 2, 10, 10, FF2400)
 gpu.rectangle(2, 2, 10, 10, 0x11FF00)
 gpu.sync()
+
+running = true
+
+local keyboard = peripheral.find("tm_keyboard")
+if keyboard then
+    keyboard.setFireNativeEvents(true)
+end
+
+print("Press q to exit")
+
+-- Главный цикл
+while running do
+    local event, keyCode = os.pullEvent("key")
+    local keyName = keys.getName(keyCode)
+    
+    print("Key code:", keyCode)
+    print("Key name (English layout):", keyName)
+    if keyCode == keys.q then
+        print("Exiting...")
+        running = false
+    end
+end
