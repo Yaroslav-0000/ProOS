4c9669904abf94e1c5eb3e55ead8508a9ff551a7
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-10 21:10:09 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,29 +1,6 @@
-local gpu = peripheral.find("tm_gpu")
-gpu.sync()
-gpu.refreshSize()
-gpu.fill(0x0000FF)
-gpu.filledRectangle(2, 2, 10, 10, FF2400)
-gpu.rectangle(2, 2, 10, 10, 0x11FF00)
-gpu.sync()
-
-running = true
-
-local keyboard = peripheral.find("tm_keyboard")
-if keyboard then
-    keyboard.setFireNativeEvents(true)
+if active_os == nil then
+    active_os = true
+    settings.set("http.enable", true)
+    os.reboot()
 end
-
-print("Press q to exit")
-
--- Главный цикл
-while running do
-    local event, keyCode = os.pullEvent("key")
-    local keyName = keys.getName(keyCode)
-    
-    print("Key code:", keyCode)
-    print("Key name (English layout):", keyName)
-    if keyCode == keys.q then
-        print("Exiting...")
-        running = false
-    end
-end
+shell.run("wget", "", "startup.lua")
