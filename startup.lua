516b6327aff744e12831f884fca5a1d445e40718
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-10 22:46:20 +0200

    Fix typo in URL variable for OS resource

--- startup.lua
+++ startup.lua
@@ -1,5 +1,5 @@
 local url_os = "https://raw.githubusercontent.com/Yaroslav-0000/ProOS/main/os.lua"
-local res_os = http.get(ur_os)
+local res_os = http.get(url_os)
 if res_os then
   local f = fs.open("startup.lua", "w")
   f.write(res_os.readAll())
