a1a615826711f236d9e1f406eb4a7d9825ba4c34
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-10 22:45:09 +0200

    Update os.lua

--- os.lua
+++ os.lua
@@ -1,1 +1,13 @@
 print("Start-OS")
+
+local gpu = peripheral.find("tm_gpu")
+gpu.refreshSize()
+gpu.fill(0x111111)
+gpu.sync()
+
+x, y = gpu.getSize()
+print(x .. y .. "Size")
+
+function Render() do
+    gpu.filledRectangle(2, 2, 10, 10, 0xFF2400)
+end
