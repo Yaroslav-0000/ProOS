22c68639f2155cde059ec8a49684911b11ced2d2
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-11 13:38:04 +0200

    Update os.lua

--- os.lua
+++ os.lua
@@ -54,10 +54,10 @@ end
 function screen_render()
     gpu.fill(0x001100)
     gpu.sync()
-    os.sleep(0.2)
+    os.sleep(0.1)
     gpu.fill(0x009159)
     gpu.sync()
-    os.sleep(0.5)
+    os.sleep(0.1)
 end
 function drivers_update()
 end
