244ceea78a5a0568f1e695ee17d5e28f4aa10fa3
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-01-11 16:28:38 +0200

    Update os.lua

--- os.lua
+++ os.lua
@@ -1,14 +1,10 @@
 print("Start-OS")
 
-gpu = peripheral.find("tm_gpu")
+mon = peripheral.find("monitor")
 local keyboard = peripheral.find("tm_keyboard")
 if keyboard then
     keyboard.setFireNativeEvents(true)
 end
-gpu.refreshSize()
-gpu.setSize(64)
-gpu.fill(0x111111)
-gpu.sync()
 
 width, height = gpu.getSize()
 print(width .. height .. "Size")
@@ -51,67 +47,6 @@ desktop_renderer_fun_s = {}
 drivers_updatede_fun_s = {}
 desktop_see = false
 
-FONT_5x8 = {
-["A"]={"01110","10001","10001","11111","10001","10001","10001","00000"},
-["B"]={"11110","10001","10001","11110","10001","10001","11110","00000"},
-["C"]={"01110","10001","10000","10000","10000","10001","01110","00000"},
-["D"]={"11110","10001","10001","10001","10001","10001","11110","00000"},
-["E"]={"11111","10000","10000","11110","10000","10000","11111","00000"},
-["F"]={"11111","10000","10000","11110","10000","10000","10000","00000"},
-["G"]={"01110","10001","10000","10011","10001","10001","01110","00000"},
-["H"]={"10001","10001","10001","11111","10001","10001","10001","00000"},
-["I"]={"01110","00100","00100","00100","00100","00100","01110","00000"},
-["J"]={"00111","00010","00010","00010","10010","10010","01100","00000"},
-["K"]={"10001","10010","10100","11000","10100","10010","10001","00000"},
-["L"]={"10000","10000","10000","10000","10000","10000","11111","00000"},
-["M"]={"10001","11011","10101","10101","10001","10001","10001","00000"},
-["N"]={"10001","10001","11001","10101","10011","10001","10001","00000"},
-["O"]={"01110","10001","10001","10001","10001","10001","01110","00000"},
-["P"]={"11110","10001","10001","11110","10000","10000","10000","00000"},
-["Q"]={"01110","10001","10001","10001","10101","10010","01101","00000"},
-["R"]={"11110","10001","10001","11110","10100","10010","10001","00000"},
-["S"]={"01111","10000","10000","01110","00001","00001","11110","00000"},
-["T"]={"11111","00100","00100","00100","00100","00100","00100","00000"},
-["U"]={"10001","10001","10001","10001","10001","10001","01110","00000"},
-["V"]={"10001","10001","10001","10001","10001","01010","00100","00000"},
-["W"]={"10001","10001","10001","10101","10101","10101","01010","00000"},
-["X"]={"10001","10001","01010","00100","01010","10001","10001","00000"},
-["Y"]={"10001","10001","01010","00100","00100","00100","00100","00000"},
-["Z"]={"11111","00001","00010","00100","01000","10000","11111","00000"}
-}
-
-
-local PIXEL_SCALE = 3  -- каждый “пиксель” символа будет 3×3 блок
-
-function drawChar(x, y, char, color)
-    local glyph = FONT_5x8[char]
-    if not glyph then return end
-
-    for row = 1, 8 do
-        local line = glyph[row]
-        for col = 1, 5 do
-            if line:sub(col, col) == "1" then
-                local px = x + (col-1)*PIXEL_SCALE
-                local py = y + (row-1)*PIXEL_SCALE
-                gpu.filledRectangle(px, py, px+PIXEL_SCALE-1, py+PIXEL_SCALE-1, color)
-            end
-        end
-    end
-end
-
-
-function drawText(x, y, text, color)
-    local cx = x
-    for i = 1, #text do
-        local ch = text:sub(i, i)
-        drawChar(cx, y, ch, color)
-        cx = cx + 6 -- 5 пикселей символ + 1 пробел
-    end
-end
-
-
-
-
 function events_chek()
     local event, keyCode = os.pullEvent("key")
     local keyName = keys.getName(keyCode)
@@ -122,13 +57,12 @@ function events_chek()
     end
 end
 function screen_render()
+    mon.clear()
     if desktop_see then
         for i, func in ipairs(desktop_renderer_fun_s) do
             func()
         end
     end
-    drawText(10, 10, "PRO OS", 0x00FF00)
-    gpu.sync()
 end
 function drivers_update()
     for i, func in ipairs(drivers_updatede_fun_s) do
@@ -140,7 +74,7 @@ function UNLimitedFunS()
     drivers_update()
 end
 function createUserMenu()
-    
+    mon.setBackgroundColor(colors.blue)
 end
 function ProOS()
     if not fs.exists("users") then
@@ -152,7 +86,7 @@ function ProOS()
         desktop_see = true
     end
     table.insert(drivers_updatede_fun_s, function()
-            gpu = peripheral.find("tm_gpu")
+            mon = peripheral.find("monitor"))
 end)
 
     table.insert(desktop_renderer_fun_s, function()
