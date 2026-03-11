112c554bb90e8d90609f8f1f59749d79076c5dd5
Yaroslav-0000 <aroslavrublevskij007@gmail.com>
2026-03-11 11:12:00 +0200

    Comment out HTTP request and enhance GPU color rendering
    Commented out the HTTP request to fetch content from a URL and added color generation logic for a graphical display.

--- os.lua
+++ os.lua
@@ -4,7 +4,7 @@ running = true
 
 local url_os = "https://raw.githubusercontent.com/Yaroslav-0000/ProOS/main/os.lua"
 
-local res_os = http.get(url_os)
+-- local res_os = http.get(url_os)
 if res_os then
     local new_content = res_os.readAll()
     res_os.close()
@@ -40,12 +40,29 @@ function handleClick(x, y, isShift)
     gpu.filledRectangle(x - 2, y - 2, 4, 4, 0x006CFA)
     gpu.sync()
 end
+
+print(gpu.getSize())
 function ProOS()
 
     gpu.refreshSize()
     gpu.setSize(64)
 
     gpu.fill(0x333333)
+
+    local W, H = gpu.getSize()
+
+    for y = 2, H - 1 do
+        for x = 2, W - 1 do
+            local t = (x + y) / (W + H)
+            local r = math.floor(((math.sin(t * math.pi * 2) * 0.5) + 0.5) * 255)
+            local g = math.floor(((math.sin(t * math.pi * 2 + 2.094) * 0.5) + 0.5) * 255)
+            local b = math.floor(((math.sin(t * math.pi * 2 + 4.188) * 0.5) + 0.5) * 255)
+
+            local color = (r * 0x10000) + (g * 0x100) + b
+
+            gpu.filledRectangle(x, y, 1, 1, color)
+        end
+    end
 
     gpu.sync()
 
@@ -57,4 +74,5 @@ function ProOS()
     end
 
 end
+
 ProOS()
